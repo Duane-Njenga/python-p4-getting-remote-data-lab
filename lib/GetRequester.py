0a3db3a127bdd82890bdbe6ecb4d0ca5882e65e7
Dwayne <dune.njenga@student.moringaschool.com>
2025-06-15 19:02:22 +0300

    Completed Lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,12 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response = self.get_response_body()
+        response_json = json.loads(response)
+        return response_json
+    
+
